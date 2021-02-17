61df0286546d1aaf74c5f54cca4ba5c631be3744
johnnie71 <johnnie.agonz71@gmail.com>
2021-02-17 15:45:33 -0500

    set products in avail and user products filtered to a certain user

--- store/reducers/products.js
+++ store/reducers/products.js
@@ -2,7 +2,7 @@ import PRODUCTS from '../../data/dummy-data';
 
 const initialState = {
     avalailableProducts: PRODUCTS,
-    userProducts: []
+    userProducts: PRODUCTS.filter(prod => prod.ownerId === 'u1')
 };
 
 export default (state = initialState, action) => {
