4b97a30eee68b82937a06299f049cd883efa206f
johnnie71 <johnnie.agonz71@gmail.com>
2021-02-17 15:48:19 -0500

    imported Provider from react redux to provide the app with everything redux

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { createStore, combineReducers } from 'redux';
-import {} from 'react-redux';
+import { Provider } from 'react-redux';
 
 export default function App() {
   return (
