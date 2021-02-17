3c54a77f1c56615fbab43cac1a77b0b8b6ac4300
johnnie71 <johnnie.agonz71@gmail.com>
2021-02-17 15:49:22 -0500

    imported products reducer

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 
+import productsReducer from './store/reducers/products';
+
 export default function App() {
   return (
     <View style={styles.container}>
