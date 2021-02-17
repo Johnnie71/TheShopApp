e856339cb0f3d2ae7a893152fa19a328376edb48
johnnie71 <johnnie.agonz71@gmail.com>
2021-02-17 15:55:39 -0500

    set provider component with store property

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ const store = createStore(rootReducer)
 
 export default function App() {
   return (
-    <Provider >
+    <Provider store={store} >
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         
