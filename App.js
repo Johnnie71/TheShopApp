86224bcb18e3f6f1b801a6532e310268fda7bc7e
johnnie71 <johnnie.agonz71@gmail.com>
2021-02-17 15:54:18 -0500

    set provider component surrounding everything else so the app can access store

--- App.js
+++ App.js
@@ -15,10 +15,12 @@ const store = createStore(rootReducer)
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider >
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        
+      </View>
+    </Provider>
   );
 }
 
