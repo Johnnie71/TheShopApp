73772ed1038b142fbfdcebb5d4086ccc502f5efc
johnnie71 <johnnie.agonz71@gmail.com>
2021-02-17 15:47:35 -0500

    imported from redux createStore and combineReducers

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStore, combineReducers } from 'redux';
+import {} from 'react-redux';
 
 export default function App() {
   return (
