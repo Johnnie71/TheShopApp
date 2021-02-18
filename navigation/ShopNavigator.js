69ebd0f47d6c582589b95543e8af9ff38f1d83af
johnnie71 <johnnie.agonz71@gmail.com>
2021-02-18 12:57:16 -0500

    added a stack to navigate to products overview screen

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -3,5 +3,7 @@ import { createStackNavigator } from 'react-navigation';
 import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen';
 
 const ProductsNavigator = createStackNavigator({
-
+    ProductsOverview: ProductsOverviewScreen
+} {
+    
 });
