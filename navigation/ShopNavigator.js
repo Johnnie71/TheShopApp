87e1ab111679a30155bd09efd99baedf63d16d55
johnnie71 <johnnie.agonz71@gmail.com>
2021-02-18 13:06:08 -0500

    added primary colors for backgroundColor

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -1,13 +1,14 @@
 import { createStackNavigator } from 'react-navigation';
 
 import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen';
+import Colors from '../constants/Colors';
 
 const ProductsNavigator = createStackNavigator({
     ProductsOverview: ProductsOverviewScreen
 }, {
     defaultNavigationOptions: {
         headerStyle: {
-            backgroundColor: 
+            backgroundColor: Colors.primary
         }
     }
 });
