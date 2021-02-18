68542bfcaf02166491e2d402d3d73dd6bc171e3e
johnnie71 <johnnie.agonz71@gmail.com>
2021-02-18 12:43:14 -0500

    created variable for useSelector to grab a certain state needed from redux store

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -3,6 +3,9 @@ import { FlatList } from 'react-native';
 import { useSelector } from 'react-redux';
 
 const ProductsOverviewScreen = props => {
+
+    const products = useSelector(state => state.products.availableProducts);
+
     return(
         <FlatList />
     );
