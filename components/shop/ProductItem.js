17477015ec32b98c3acf82bd3b6626a7534ffb2b
johnnie71 <johnnie.agonz71@gmail.com>
2021-02-19 15:13:31 -0500

    finished css for layout

--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import { View, Text, Image, StyleSheet, Button } from 'react-native';
+import Colors from '../../constants/Colors'
 
 const ProductItem = props => {
     return (
         <View style={styles.product}>
-            <Image style={styles.image} source={{uri: props.image}} />
-            <Text style={styles.title}>{props.title}</Text>
-            <Text style={styles.price}>${props.price.toFixed(2)}</Text>
+            <View style={styles.imageContainer}>
+             <Image style={styles.image} source={{uri: props.image}} />
+            </View>
+            <View style={styles.details}>
+                <Text style={styles.title}>{props.title}</Text>
+                <Text style={styles.price}>${props.price.toFixed(2)}</Text>
+            </View>
             <View style={styles.actions}>
-                <Button title ="View Details" onPress={props.onViewDetail} />
-                <Button title ="To Cart" onPress={props.onAddToCart} />
+                <Button color={Colors.primary} title ="View Details" onPress={props.onViewDetail} />
+                <Button color={Colors.primary} title ="To Cart" onPress={props.onAddToCart} />
             </View>
         </View>
     )
@@ -29,7 +34,7 @@ const styles = StyleSheet.create({
     },
     image: {
         width: '100%',
-        height: '60%'
+        height: '100%'
     },
     title: {
         fontSize: 18,
@@ -42,7 +47,21 @@ const styles = StyleSheet.create({
     actions: {
         flexDirection: 'row',
         justifyContent:'space-between',
-        alignItems: 'center'
+        alignItems: 'center',
+        height: '25%',
+        paddingHorizontal: 20
+    },
+    details: {
+        alignItems: 'center',
+        height: '15%',
+        padding: 10
+    },
+    imageContainer: {
+        width: '100%',
+        height: '60%',
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
+        overflow: 'hidden'
     }
 });
 
