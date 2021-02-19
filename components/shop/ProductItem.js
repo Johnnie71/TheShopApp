e8096c7dad084af3059db946f1d3b50e17d26fc5
johnnie71 <johnnie.agonz71@gmail.com>
2021-02-19 14:36:10 -0500

    shadowcolor for ios and elevation in styles for android

--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -16,7 +16,13 @@ const ProductItem = props => {
 };
 
 const styles = StyleSheet.create({
-
+    product: {
+        shadowColor: 'black',
+        shadowOpacity: 0.26,
+        shadowOffset: {width: 0, height: 2},
+        shadowRadius: 8,
+        elevation: 8
+    }
 });
 
 export default ProductItem;
