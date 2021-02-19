bae57fdfd69cfee30c9ace2b395964fc6ccc5317
johnnie71 <johnnie.agonz71@gmail.com>
2021-02-19 14:33:26 -0500

    set up basic layout for what the item will show

--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -2,7 +2,17 @@ import React from 'react';
 import { View, Text, Image, StyleSheet } from 'react-native';
 
 const ProductItem = props => {
-
+    return (
+        <View>
+            <Image />
+            <Text>TITLE</Text>
+            <Text>PRICE</Text>
+            <View>
+                <Button title ="View Details" />
+                <Button title ="To Cart" />
+            </View>
+        </View>
+    )
 };
 
 const styles = StyleSheet.create({
