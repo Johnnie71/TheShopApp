d3cb28b96b6bd3c3f40197fb851e0515b8357253
johnnie71 <johnnie.agonz71@gmail.com>
2021-02-20 17:49:09 -0500

    basic view to see

--- screens/shop/ProductDetailScreen.js
+++ screens/shop/ProductDetailScreen.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { ScrollView, View, Text, Image, Button, StyleSheet, StatusBar } from 'react-native';
 
 const ProductDetailsScreen = props => {
-
+    return (
+        <View>
+            <Text>The Product Detail Screen!</Text>
+        </View>
+    )
 };
 
 const styles = StyleSheet.create({
