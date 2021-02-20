6d13860240fae0774551be9c89abc339c7d51b4f
johnnie71 <johnnie.agonz71@gmail.com>
2021-02-20 17:47:00 -0500

    basic setup

--- screens/shop/ProductDetailScreen.js
+++ screens/shop/ProductDetailScreen.js
@@ -1,2 +1,12 @@
 import React from 'react';
-import { ScrollView, View, Text, Image, Button, StyleSheet } from 'react-native';
+import { ScrollView, View, Text, Image, Button, StyleSheet, StatusBar } from 'react-native';
+
+const ProductDetailsScreen = props => {
+
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default ProductDetailsScreen;
