f89ee5e6ca83e6d9a507e5343fb3ce9fd9bf0cd7
johnnie71 <johnnie.agonz71@gmail.com>
2021-02-20 18:01:04 -0500

    extracting the productId from navigation props

--- screens/shop/ProductDetailScreen.js
+++ screens/shop/ProductDetailScreen.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { ScrollView, View, Text, Image, Button, StyleSheet, StatusBar } from 'react-native';
 
 const ProductDetailsScreen = props => {
+
+    const productId = props.navigation.getParam('productId');
+
+
     return (
         <View>
             <Text>The Product Detail Screen!</Text>
