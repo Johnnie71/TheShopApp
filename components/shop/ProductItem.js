e1d5725a2976bfcc28d1e15ce6be2ef263470e6a
johnnie71 <johnnie.agonz71@gmail.com>
2021-02-23 15:42:22 -0500

    moved touchableOpacity inside view

--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -13,21 +13,21 @@ const ProductItem = props => {
 
 
     return (
-        <TouchableCmp onPress={props.onViewDetail} useForeground >
         <View style={styles.product}>
-            <View style={styles.imageContainer}>
-             <Image style={styles.image} source={{uri: props.image}} />
-            </View>
-            <View style={styles.details}>
-                <Text style={styles.title}>{props.title}</Text>
-                <Text style={styles.price}>${props.price.toFixed(2)}</Text>
-            </View>
-            <View style={styles.actions}>
-                <Button color={Colors.primary} title ="View Details" onPress={props.onViewDetail} />
-                <Button color={Colors.primary} title ="To Cart" onPress={props.onAddToCart} />
-            </View>
+            <TouchableCmp onPress={props.onViewDetail} useForeground >
+                <View style={styles.imageContainer}>
+                <Image style={styles.image} source={{uri: props.image}} />
+                </View>
+                <View style={styles.details}>
+                    <Text style={styles.title}>{props.title}</Text>
+                    <Text style={styles.price}>${props.price.toFixed(2)}</Text>
+                </View>
+                <View style={styles.actions}>
+                    <Button color={Colors.primary} title ="View Details" onPress={props.onViewDetail} />
+                    <Button color={Colors.primary} title ="To Cart" onPress={props.onAddToCart} />
+                </View>
+            </TouchableCmp>
         </View>
-        </TouchableCmp>
     )
 };
 
