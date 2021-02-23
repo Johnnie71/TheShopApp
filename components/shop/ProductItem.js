b01c23465b3d1f24e18ca162442faf42aad3add1
johnnie71 <johnnie.agonz71@gmail.com>
2021-02-23 15:39:46 -0500

    changed touchableOpacity to TouchableCmp

--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -13,7 +13,7 @@ const ProductItem = props => {
 
 
     return (
-        <TouchableOpacity onPress={props.onViewDetail} >
+        <TouchableCmp onPress={props.onViewDetail} >
         <View style={styles.product}>
             <View style={styles.imageContainer}>
              <Image style={styles.image} source={{uri: props.image}} />
@@ -27,7 +27,7 @@ const ProductItem = props => {
                 <Button color={Colors.primary} title ="To Cart" onPress={props.onAddToCart} />
             </View>
         </View>
-        </TouchableOpacity>
+        </TouchableCmp>
     )
 };
 
