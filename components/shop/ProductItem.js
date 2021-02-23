b1505b98068ac437ad2496afafa825b7453ce0a8
johnnie71 <johnnie.agonz71@gmail.com>
2021-02-23 15:36:22 -0500

    imported platform

--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet, Button, TouchableOpacity, TouchableNativeFeedback } from 'react-native';
+import { View, Text, Image, StyleSheet, Button, TouchableOpacity, TouchableNativeFeedback, Platform } from 'react-native';
 import Colors from '../../constants/Colors';
 
 
