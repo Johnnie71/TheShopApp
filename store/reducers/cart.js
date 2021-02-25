dd48309ef1865c0728545b28b125700d69e465a0
johnnie71 <johnnie.agonz71@gmail.com>
2021-02-25 18:24:09 -0500

    add to cart type using switch case

--- store/reducers/cart.js
+++ store/reducers/cart.js
@@ -1,8 +1,12 @@
+
 const initialState = {
     items: {},
     totalAmount: 0
 };
 
 export default (state = initialState, action) => {
+    switch (action.type){
+        case ADD_TO_CART:
+    }
     return state;
 };
