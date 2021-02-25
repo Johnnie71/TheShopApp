5e1435424a90adb621ea519e3aab27f1b14c569e
johnnie71 <johnnie.agonz71@gmail.com>
2021-02-25 18:26:31 -0500

    storing the action type product in a constant

--- store/reducers/cart.js
+++ store/reducers/cart.js
@@ -7,7 +7,8 @@ const initialState = {
 
 export default (state = initialState, action) => {
     switch (action.type){
-        case ADD_TO_CART:
+        case ADD_TO_CART: 
+            const addedProduct = action.product;
     }
     return state;
 };
