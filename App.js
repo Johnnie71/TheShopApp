4992d9c681331e8994caed7d88c8f723a7870493
johnnie71 <johnnie.agonz71@gmail.com>
2021-02-25 17:12:10 -0500

    npm install expo-font and imported it for use

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import { AppLoading } from 'expo';
+import * as Font from 'expo-font';
 
 import productsReducer from './store/reducers/products';
 import ShopNavigator from './navigation/ShopNavigator';
