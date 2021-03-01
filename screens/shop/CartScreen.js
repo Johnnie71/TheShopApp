1b059e3978ac741d1e5a381e7418df42454fa407
johnnie71 <johnnie.agonz71@gmail.com>
2021-03-01 13:24:13 -0500

    set up for View and Text

--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { View, Text, StyleSheet, Flatlist, Button } from 'react-native';
 
-const CartScreen = props => {};
+const CartScreen = props => {
+    return (
+        <View>
+            <View>
+                <Text>Total: <Text>${19.99}</Text></Text>
+            </View>
+        </View>
+    )
+};
 
 const styles = StyleSheet.create({
-
+    
 });
 
 export default CartScreen;
