8943f1ccd7b1a53413c822b67c24cb856bda594c
johnnie71 <johnnie.agonz71@gmail.com>
2021-03-01 13:28:10 -0500

    imported useSelector to get data from Redux

--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, StyleSheet, Flatlist, Button } from 'react-native';
+import { useSelector } from 'react-redux';
 
 const CartScreen = props => {
     return (
