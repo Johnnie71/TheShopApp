32b726db1db6fda107ed181a711bc83289010b70
johnnie71 <johnnie.agonz71@gmail.com>
2021-03-02 15:45:07 -0500

    imported Ionicons

--- components/shop/CartItem.js
+++ components/shop/CartItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 const CartItem = props => {
     return (
