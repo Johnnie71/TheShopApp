e900780576e3af5c3ba0de9e310ec6222211de4c
johnnie71 <johnnie.agonz71@gmail.com>
2021-03-02 15:44:15 -0500

    imported Touchable Opacity to make own touch set up

--- components/shop/CartItem.js
+++ components/shop/CartItem.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 
 const CartItem = props => {
     return (
         <View>
-            
+            <Text>
+                <Text>QTY</Text><Text>TITLE</Text>
+            </Text>
+            <View>
+                <Text>$AMT</Text>
+
+            </View>
         </View>
     )
 };
