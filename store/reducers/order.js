234b305ee186183cdb8415417db9df5e64f8fdf8
johnnie71 <johnnie.agonz71@gmail.com>
2021-03-04 15:29:47 -0500

    imported Order class and created a new Order with case ADD_ORDER

--- store/reducers/order.js
+++ store/reducers/order.js
@@ -1,4 +1,5 @@
 import { ADD_ORDER } from "../actions/order";
+import Order from '../../models/order';
 
 const initialState = {
     orders: []
@@ -8,7 +9,7 @@ export default (state = initialState, action) => {
 
     switch(action.type) {
         case ADD_ORDER: 
-            
+            const newOrder = new Order(); 
     }
 
     return state;
