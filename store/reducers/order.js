9e11dcd2300313211a2db0b04404eb5e0e483156
johnnie71 <johnnie.agonz71@gmail.com>
2021-03-04 15:33:33 -0500

    adjusted code for readability

--- store/reducers/order.js
+++ store/reducers/order.js
@@ -9,7 +9,11 @@ export default (state = initialState, action) => {
 
     switch(action.type) {
         case ADD_ORDER: 
-            const newOrder = new Order(new Date().toString(), action.orderData.items, action.orderData.amount); 
+            const newOrder = new Order(new Date().toString(), 
+            action.orderData.items, 
+            action.orderData.amount, 
+            new Date()
+            ); 
     }
 
     return state;
