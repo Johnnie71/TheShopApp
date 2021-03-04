8284d55a384fa7be5ec06598ebf2a5637aa18e95
johnnie71 <johnnie.agonz71@gmail.com>
2021-03-04 15:32:34 -0500

    setting up actions for add order to get the items and the total amount from actions

--- store/reducers/order.js
+++ store/reducers/order.js
@@ -9,7 +9,7 @@ export default (state = initialState, action) => {
 
     switch(action.type) {
         case ADD_ORDER: 
-            const newOrder = new Order(new Date().toString()); 
+            const newOrder = new Order(new Date().toString(), action.orderData.items, action.orderData.amount); 
     }
 
     return state;
