88fa21464da2b5233a02d1419d1860e883741e7d
johnnie71 <johnnie.agonz71@gmail.com>
2021-03-04 15:30:48 -0500

    using a built in javascript method to get the current date for the order

--- store/reducers/order.js
+++ store/reducers/order.js
@@ -9,7 +9,7 @@ export default (state = initialState, action) => {
 
     switch(action.type) {
         case ADD_ORDER: 
-            const newOrder = new Order(); 
+            const newOrder = new Order(new Date().toString()); 
     }
 
     return state;
