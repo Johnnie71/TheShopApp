6c72acb4550ea9c5191e41701bb13bb608eb2d0a
johnnie71 <johnnie.agonz71@gmail.com>
2021-03-05 15:04:58 -0500

    basic setup for file

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import { View, Text, StyleSheet, FlatList } from 'react-native';
+
+const OrdersScreen = props => {
+
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default OrdersScreen;
