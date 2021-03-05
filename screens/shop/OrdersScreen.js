515a07dc3cf310288713ad33563d8582eb5d1f37
johnnie71 <johnnie.agonz71@gmail.com>
2021-03-05 15:14:35 -0500

    used FlatList in return and used the const orders for the data within

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { StyleSheet, FlatList } from 'react-native';
+import { StyleSheet, FlatList, } from 'react-native';
 import { useSelector } from 'react-redux';
 
 const OrdersScreen = props => {
      const orders = useSelector(state => state.orders.orders);
+
+     return (
+         <FlatList data={orders} keyExtractor={item => item.id} renderItem={itemData => {}} />
+     )
 };
 
 const styles = StyleSheet.create({
