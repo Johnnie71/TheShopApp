8ad201607a4fdf1dcd8e3e8a7b85aac51f03c1e2
johnnie71 <johnnie.agonz71@gmail.com>
2021-03-09 17:16:46 -0500

    imported ionicons and addd attributes

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -1,6 +1,7 @@
 import { createStackNavigator } from '@react-navigation/stack';
 // import { createDrawerNavigator} from '@react-navigation/drawer';
 import { Platform } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 import ProductsOverviewScreen, { screenOptions as productsOverviewScreenOptions } from '../screens/shop/ProductsOverviewScreen';
 import ProductDetailsScreen, { screenOptions as productDetailScreenOptions } from '../screens/shop/ProductDetailScreen';
@@ -59,7 +60,13 @@ const ProductsNavigator = () => {
 //     Orders: OrdersScreen,
 // }, {
 //    navigationOptions: {
-//          drawerIcon: drawerConfig => <Ionicons />
+//       drawerIcon: drawerConfig => (
+//                      <Ionicons 
+//                           name={Platform.OS === 'android' ? 'md-create' : 'ios-create'} 
+//                           size={23}
+//                           color={drawerConfig.tintColor}
+//                       />
+//                   )
 // },
 //     defaultNavigationOptions: defaultNavOptions
 // });
