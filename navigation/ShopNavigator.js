300925781b86923ac3b5c23f543bfee260057fc8
johnnie71 <johnnie.agonz71@gmail.com>
2021-03-09 17:21:27 -0500

    importing react from react to be able to use JSX syntax

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 // import { createDrawerNavigator} from '@react-navigation/drawer';
 import { Platform } from 'react-native';
