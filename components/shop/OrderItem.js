f3578b3a9d819bbc3b1623a1667a62418004d3a6
johnnie71 <johnnie.agonz71@gmail.com>
2021-03-10 18:24:39 -0500

    imported cartitem

--- components/shop/OrderItem.js
+++ components/shop/OrderItem.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
+import CartItem from './CartItem';
 
 const OrderItem = props => {
-
+    return (
+        <View>
+            
+        </View>
+    )
 };
 
 const styles = StyleSheet.create({
