0a820e0bcfe9313cf949f424a5af0a80764afd37
johnnie71 <johnnie.agonz71@gmail.com>
2021-03-11 14:34:59 -0500

    imported ProductItem to use in flatList data

--- screens/user/UserProductScreen.js
+++ screens/user/UserProductScreen.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { FlatList } from 'react-native';
 
+import ProductItem from '../../components/shop/ProductItem';
+
 const UserProductsScreen = props => {
     return (
-
+        <FlatList />
     )
 };
 
