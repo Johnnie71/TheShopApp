45aff3e37f301b7bfe506ac5deffe3a510ae14be
johnnie71 <johnnie.agonz71@gmail.com>
2021-03-10 19:01:04 -0500

    changed date to get date from order class

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -12,7 +12,13 @@ const OrdersScreen = props => {
          <FlatList 
          data={orders} 
          keyExtractor={item => item.id} 
-         renderItem={itemData => <OrderItem amount={itemData.item.totalAmount} date={itemData.item.date} /> } />
+         renderItem={itemData => (
+                <OrderItem 
+                    amount={itemData.item.totalAmount} 
+                    date={itemData.item.readableDate} 
+                />
+            ) } 
+         />
      );
 };
 
