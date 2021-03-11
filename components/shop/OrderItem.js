46443171fc318ab65d1aad4ef007b1b6bb2a9063
johnnie71 <johnnie.agonz71@gmail.com>
2021-03-11 14:03:11 -0500

    imported useState and created a const in order to change when button press

--- components/shop/OrderItem.js
+++ components/shop/OrderItem.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 import { Colors } from '../../constants/Colors';
 import CartItem from './CartItem';
 
 const OrderItem = props => {
+
+    const [showDetails, setShowDetails] = useState(false);
+
     return (
         <View style={styles.orderItem} >
             <View style={styles.summary} >
