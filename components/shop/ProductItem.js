6a5983fa93b11d3f6e9f4b746ef4f4c91d03cc3b
johnnie71 <johnnie.agonz71@gmail.com>
2021-03-12 14:09:09 -0500

    got rid of colors since they will not be used

--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { View, Text, Image, StyleSheet, Button, TouchableOpacity, TouchableNativeFeedback, Platform } from 'react-native';
-import Colors from '../../constants/Colors';
 
 
 const ProductItem = props => {
