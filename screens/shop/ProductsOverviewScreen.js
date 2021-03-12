4426e82247940e6c385eef9d491ff658c0081e02
johnnie71 <johnnie.agonz71@gmail.com>
2021-03-12 14:19:16 -0500

    used onSelect prop

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -28,8 +28,8 @@ const ProductsOverviewScreen = props => {
             <ProductItem image={itemData.item.imageUrl} 
             title={itemData.item.title} 
             price={itemData.item.price} 
-            onViewDetail={() => {
-                
+            onSelect={() => {
+                selectItemHandler(itemData.item.id, itemData.item.title)
             }} 
             onAddToCart={() => {
                 dispatch(cartActions.addToCart(itemData.item));
