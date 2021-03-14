3515b8c3d7ced517c94559e63c76cec1e86aa5ce
johnnie71 <johnnie.agonz71@gmail.com>
2021-03-14 17:33:06 -0400

    added more inputs

--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -4,6 +4,7 @@ import { View, ScrollView, Text, TextInput, StyleSheet } from 'react-native';
 const EditProductScreen = props => {
     return (
         <ScrollView>
+         <View style={styles.form}>
             <View style={styles.formControl}>
                 <Text style={styles.label}>Title</Text>
                 <TextInput style={styles.input} />
@@ -12,6 +13,15 @@ const EditProductScreen = props => {
                 <Text style={styles.label}>Image URL</Text>
                 <TextInput style={styles.input} />
             </View>
+            <View style={styles.formControl}>
+                <Text style={styles.label}>Price</Text>
+                <TextInput style={styles.input} />
+            </View>
+            <View style={styles.formControl}>
+                <Text style={styles.label}>Description</Text>
+                <TextInput style={styles.input} />
+            </View>
+          </View>
         </ScrollView>
     )
 
