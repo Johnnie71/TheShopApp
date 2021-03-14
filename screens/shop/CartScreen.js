f5eda884046d92c839b8ae3e8cf7486229c0233e
johnnie71 <johnnie.agonz71@gmail.com>
2021-03-14 17:21:10 -0400

    Math.round to get rid of negative sign when total is zero

--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -29,7 +29,8 @@ const CartScreen = props => {
         <View style={styles.screen}>
             <View style={styles.summary}>
                 <Text style={styles.summaryText}>
-                    Total: <Text style={styles.amount}>${cartTotalAmount.toFixed(2)}</Text>
+                    Total: {' '}
+                    <Text style={styles.amount}>${Math.round(cartTotalAmount.toFixed(2) * 100) / 100}</Text>
                 </Text>
                 <Button 
                   color='#5499C7' 
