b21cb22af308f8342ed8fc3f70589b9ffab9cb08
johnnie71 <johnnie.agonz71@gmail.com>
2021-03-15 16:54:50 -0400

    create export for create product

--- store/actions/products.js
+++ store/actions/products.js
@@ -1,5 +1,7 @@
 export const DELETE_PRODUCT = 'DELETE_PRODUCT';
+export const CREATE_PRODUCT = 'CREATE_PRODUCT';
 
 export const deleteProduct = productId => {
     return { type: DELETE_PRODUCT, pid: productId }
 };
+
