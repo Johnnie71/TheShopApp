31e20c373c26be4273580efee1502b7d632673db
johnnie71 <johnnie.agonz71@gmail.com>
2021-03-15 16:14:51 -0400

    created styles for screen

--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -28,7 +28,23 @@ const EditProductScreen = props => {
 };
 
 const styles = StyleSheet.create({
+    form: {
+        margin: 20,
 
+    },
+    formControl: {
+        width: '100%'
+    },
+    label: {
+        fontFamily: 'open-sans-bold',
+        marginVertical: 8
+    },
+    input: {
+        paddingHorizontal: 2,
+        paddingVertical: 5,
+        borderBottomColor: '#ccc',
+        borderBottomWidth: 1
+    }
 });
 
 export default EditProductScreen;
