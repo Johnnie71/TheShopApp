7923ea12d8614aede7c92a8bd794eb3e6447ce4f
johnnie71 <johnnie.agonz71@gmail.com>
2021-03-16 14:36:57 -0400

    added things

--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -29,7 +29,7 @@ const EditProductScreen = props => {
                 productActions.createProduct(title, description, imageUrl, +price)
             );
         }
-    }, []);
+    }, [dispatch, productId, title, description, imageUrl, price]);
 
     useEffect(() => {
         props.navigation.setParams({submit: submitHandler })
