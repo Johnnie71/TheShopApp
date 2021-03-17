208e975dd1959674e9b12705deed785b7ec51409
johnnie71 <johnnie.agonz71@gmail.com>
2021-03-17 16:19:55 -0400

    added css for card

--- components/shop/UI/Card.js
+++ components/shop/UI/Card.js
@@ -3,14 +3,22 @@ import { View, StyleSheet } from 'react-native';
 
 const Card = props => {
     return(
-        <View>
+        <View style={styles.card} >
             {props.children}
         </View>
     )
 };
 
 const styles = StyleSheet.create({
-
+    card: {
+        shadowColor: 'black',
+        shadowOpacity: 0.26,
+        shadowOffset: {width: 0, height: 2},
+        shadowRadius: 8,
+        elevation: 8,
+        borderRadius: 10,
+        backgroundColor: 'white',
+    }
 });
 
 export default Card;
