d96addc659648f02b5e2b725ada344d92e41602c
johnnie71 <johnnie.agonz71@gmail.com>
2021-03-18 15:08:17 -0400

    set a state for title validation

--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -9,6 +9,7 @@ const EditProductScreen = props => {
 
     
     const [title, setTitle] = useState(editedProduct ? editedProduct.title : '');
+    const [titleIsValid, setTitleIsValid] = useState(false);
     const [imageUrl, setImageUrl] = useState(editedProduct ? editedProduct.imageUrl : '');
     const [price, setPrice] = useState('');
     const [description, setDescription] = useState(editedProduct ? editedProduct.description : '');
