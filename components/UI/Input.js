990679682e0123b95da6d78fe125f1555559504c
johnnie71 <johnnie.agonz71@gmail.com>
2021-03-19 15:42:12 -0400

    set up input.js

--- components/UI/Input.js
+++ components/UI/Input.js
@@ -1,2 +1,9 @@
 import React from 'react';
 import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { defaultOnOverflowMenuPress } from 'react-navigation-header-buttons';
+
+const Input = props => {};
+
+const styles = StyleSheet.create({});
+
+export default Input;
