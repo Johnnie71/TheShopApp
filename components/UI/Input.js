3d5330e1e83623c411459d6926a44e4c6d81eccc
johnnie71 <johnnie.agonz71@gmail.com>
2021-03-19 15:44:34 -0400

    cut from editProductScreen and pasted into here

--- components/UI/Input.js
+++ components/UI/Input.js
@@ -2,7 +2,23 @@ import React from 'react';
 import { View, Text, TextInput, StyleSheet } from 'react-native';
 
 
-const Input = props => {};
+const Input = props => {
+    return (
+        <View style={styles.formControl}>
+                <Text style={styles.label}>Title</Text>
+                <TextInput 
+                    style={styles.input}  
+                    value={formState.inputValues.title} 
+                    onChangeText={textChangeHandler.bind(this, 'title')}
+                    keyboardType='default'
+                    autoCapitalize='sentences'
+                    autoCorrect
+                    returnKeyType='next'
+                />
+                {!formState.inputValidities.title && <Text>Please enter a valid title!</Text>}
+            </View>
+    )
+};
 
 const styles = StyleSheet.create({});
 
