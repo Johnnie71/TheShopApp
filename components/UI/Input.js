2b13c2121840da8a324863593bb4645628686857
johnnie71 <johnnie.agonz71@gmail.com>
2021-03-19 15:42:30 -0400

    deleted it was not needed

--- components/UI/Input.js
+++ components/UI/Input.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text, TextInput, StyleSheet } from 'react-native';
-import { defaultOnOverflowMenuPress } from 'react-navigation-header-buttons';
+
 
 const Input = props => {};
 
