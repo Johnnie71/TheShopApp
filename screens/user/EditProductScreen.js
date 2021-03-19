d06e58d61b32fce485edfec16f18aeaf36224ef7
johnnie71 <johnnie.agonz71@gmail.com>
2021-03-19 15:45:33 -0400

    cut to use in Input.js

--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -174,19 +174,6 @@ const styles = StyleSheet.create({
     form: {
         margin: 20,
 
-    },
-    formControl: {
-        width: '100%'
-    },
-    label: {
-        fontFamily: 'open-sans-bold',
-        marginVertical: 8
-    },
-    input: {
-        paddingHorizontal: 2,
-        paddingVertical: 5,
-        borderBottomColor: '#ccc',
-        borderBottomWidth: 1
     }
 });
 
