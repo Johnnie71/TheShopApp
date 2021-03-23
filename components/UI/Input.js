230ee7996c5c215b4025fde58f8ccd1770895db0
johnnie71 <johnnie.agonz71@gmail.com>
2021-03-23 18:12:58 -0400

    useEffect to affect on input

--- components/UI/Input.js
+++ components/UI/Input.js
@@ -31,6 +31,10 @@ const Input = props => {
         touched: false
     });
 
+    useEffect(() => {
+        props.onInputChange(inputState.value, inputState.isValid)
+    })
+
     const textChangeHandler = text => {
         const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         let isValid = true;
