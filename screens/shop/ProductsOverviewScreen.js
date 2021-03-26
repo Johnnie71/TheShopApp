b08c2492b4bcff00fecdf0fcd836aa32bf206a7e
johnnie71 <johnnie.agonz71@gmail.com>
2021-03-26 18:51:20 -0400

    useEffect to fetch

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -6,6 +6,7 @@ import HeaderButton from '../../components/UI/HeaderButton';
 
 import ProductItem from '../../components/shop/ProductItem';
 import * as cartActions from '../../store/actions/cart';
+import * as productActions from '../../store/actions/products';
 import Colors from '../../constants/Colors';
 
 const ProductsOverviewScreen = props => {
@@ -13,7 +14,9 @@ const ProductsOverviewScreen = props => {
     const products = useSelector(state => state.products.availableProducts);
     const dispatch = useDispatch();
 
-    useEffect();
+    useEffect(() => {
+        dispatch(productActions.fetchProducts());
+    }, [dispatch]);
 
     const selectItemHandler = (id, title) => {
         props.navigation.navigate('ProductDetail', { 
