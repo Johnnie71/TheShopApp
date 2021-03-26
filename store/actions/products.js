ecff6799b813c2b6557ea314067b0c5525f5e80e
johnnie71 <johnnie.agonz71@gmail.com>
2021-03-26 18:42:55 -0400

    set up fetch

--- store/actions/products.js
+++ store/actions/products.js
@@ -3,6 +3,12 @@ export const CREATE_PRODUCT = 'CREATE_PRODUCT';
 export const UPDATE_PRODUCT = 'UPDATE_PRODUCT';
 export const SET_PRODUCTS = 'SET_PRODUCTS'
 
+export const fetchProducts = () => {
+  return dispatch => {
+    dispatch({type: SET_PRODUCTS, products: []})
+  }
+}
+
 export const deleteProduct = productId => {
     return { type: DELETE_PRODUCT, pid: productId }
 };
