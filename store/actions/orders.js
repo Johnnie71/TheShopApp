711d87fde1a30c377fd4e8c9e85baee2b150d00b
johnnie71 <johnnie.agonz71@gmail.com>
2021-04-01 15:40:23 -0400

    changed url address to be associated with storage

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -4,18 +4,21 @@ export const addOrder = (cartItems, totalAmount) => {
 
     return async dispatch => {
 
-        const response = await fetch('https://shopapp-759b2-default-rtdb.firebaseio.com/products.json', {
-        method: "POST",
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          title,
-          description,
-          imageUrl,
-          price
-        })
-      });
+        const response = await fetch(
+            'https://shopapp-759b2-default-rtdb.firebaseio.com/orders.json',
+             {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    title,
+                    description,
+                    imageUrl,
+                    price
+                })
+             }
+        );
 
       if(!response.ok) {
           throw new Error('Something went wrong!');
