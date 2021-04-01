4e975c63742f09e69fc8c9ef53f0a45dcbc34196
johnnie71 <johnnie.agonz71@gmail.com>
2021-04-01 15:11:36 -0400

    set if statement for isLoading to show loading spinner

--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -13,6 +13,7 @@ import HeaderButton from '../../components/UI/HeaderButton';
 import { useDispatch, useSelector } from 'react-redux';
 import * as productActions from '../../store/actions/products';
 import Input from '../../components/UI/Input';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 const FORM_INPUT_UPDATE = 'FORM_INPUT_UPDATE'
 
@@ -119,6 +120,14 @@ const EditProductScreen = props => {
             input: inputIdentifier
         });
     },[dispatchFormState]);
+
+    if(isLoading) {
+        return (
+            <View>
+                <ActivityIndicator size='large' color={Colors.primary} />
+            </View>
+        )
+    }
 
     return (
         <KeyboardAvoidingView 
