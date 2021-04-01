c653073ffc2a078f37df5f5e148f0d1367d3299a
johnnie71 <johnnie.agonz71@gmail.com>
2021-04-01 15:37:28 -0400

    created post request for storing order to server

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -3,6 +3,27 @@ export const ADD_ORDER = 'ADD_ORDER';
 export const addOrder = (cartItems, totalAmount) => {
 
     return async dispatch => {
+
+        const response = await fetch('https://shopapp-759b2-default-rtdb.firebaseio.com/products.json', {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          title,
+          description,
+          imageUrl,
+          price
+        })
+      });
+
+      if(!response.ok) {
+          throw new Error('Something went wrong!');
+      }
+
+      const responseData = await response.json();
+
+
         dispatch ({ 
             type: ADD_ORDER, orderData: 
             { items: cartItems, amount: totalAmount} 
