5e63414b091dbb952d7eb8b3bec2a3a36dd3d2ef
johnnie71 <johnnie.agonz71@gmail.com>
2021-04-02 18:15:24 -0400

    granted access to useDispatch through a variable

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -7,6 +7,7 @@ import OrderItem from '../../components/shop/OrderItem';
 
 const OrdersScreen = props => {
      const orders = useSelector(state => state.orders.orders);
+     const dispatch = useDispatch();
 
      return (
          <FlatList 
