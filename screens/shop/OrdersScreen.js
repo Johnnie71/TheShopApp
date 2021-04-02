580981c63c3a70d9946bd0537e105006ca7076cd
johnnie71 <johnnie.agonz71@gmail.com>
2021-04-02 18:27:01 -0400

    imported react native View and StyleSheet

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -1,5 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { FlatList, Text, Platform, ActivityIndicator } from 'react-native';
+import { 
+    View, 
+    FlatList, 
+    Text, 
+    Platform, 
+    ActivityIndicator, 
+    StyleSheet 
+} from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import HeaderButton from '../../components/UI/HeaderButton';
