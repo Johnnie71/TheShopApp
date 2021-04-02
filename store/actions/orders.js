51fad5f16d6664c05ba642dfd40f263df10b7f46
johnnie71 <johnnie.agonz71@gmail.com>
2021-04-02 17:59:25 -0400

    copied try fetch from products action

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -3,6 +3,30 @@ export const SET_ORDERS = 'SET_ORDERS'
 
 export const fetchOrders = () => {
     return async dispatch => {
+        try {
+            const response = await fetch(
+              'https://shopapp-759b2-default-rtdb.firebaseio.com/orders/u1.json'
+            );
+      
+            if (!response.ok)  {
+              throw new Error('Something went wrong!');
+            }
+        
+            const responseData = await response.json();
+            const loadedProducts = [];
+        
+            for(const key in responseData){
+              loadedProducts.push(
+                new Product(
+                  key, 
+                  'u1', 
+                  responseData[key].title, 
+                  responseData[key].imageUrl, 
+                  responseData[key].description, 
+                  responseData[key].price
+                ) 
+              );
+            }
         dispatch({type: SET_ORDERS, orders: []});
     }
 };
