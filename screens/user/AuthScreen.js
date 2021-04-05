7403f133a3eebda680c3a6414f213ea1ee19c9ca
johnnie71 <johnnie.agonz71@gmail.com>
2021-04-05 16:00:23 -0400

    created functional component

--- screens/user/AuthScreen.js
+++ screens/user/AuthScreen.js
@@ -2,3 +2,7 @@ import React from 'react';
 import { ScrollView, Text, View, KeyboardAvoidingView, StyleSheet } from 'react-native';
 
 import Input from '../../components/UI/Input';
+
+const AuthScreen = props => {
+
+};
