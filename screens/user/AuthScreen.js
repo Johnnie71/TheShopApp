bf56451ae2324739bb01cc437c80477c51237de1
johnnie71 <johnnie.agonz71@gmail.com>
2021-04-05 16:12:06 -0400

    imported Card component

--- screens/user/AuthScreen.js
+++ screens/user/AuthScreen.js
@@ -2,9 +2,12 @@ import React from 'react';
 import { ScrollView, Text, View, KeyboardAvoidingView, StyleSheet } from 'react-native';
 
 import Input from '../../components/UI/Input';
+import Card from '../../components/UI/Card';
 
 const AuthScreen = props => {
+    return (
 
+    )
 };
 
 const styles = StyleSheet.create({
