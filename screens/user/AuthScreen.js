61fae9bf47bbe3a4f475cc928805d947e25eddc1
johnnie71 <johnnie.agonz71@gmail.com>
2021-04-08 09:59:52 -0400

    added style to linear gradient

--- screens/user/AuthScreen.js
+++ screens/user/AuthScreen.js
@@ -19,7 +19,7 @@ const AuthScreen = props => {
             keyboardVerticalOffset={50} 
             style={styles.screen}
         >
-            <LinearGradient colors={['#ffedff', '#ffe3ff']} >
+            <LinearGradient colors={['#ffedff', '#ffe3ff']} style={styles.gradient} >
                 <Card style={styles.authContainer}>
                     <ScrollView>
                         <Input 
