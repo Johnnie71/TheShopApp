ee2e804466cac1c7da85df127ded90bdd0b7a5b0
johnnie71 <johnnie.agonz71@gmail.com>
2021-04-08 10:50:14 -0400

    set up method and header for fetch

--- store/actions/auth.js
+++ store/actions/auth.js
@@ -2,7 +2,15 @@ export const SIGNUP = 'SIGNUP';
 
 export const signup = (email, password) => {
     return async dispatch => {
-        fetch("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCxH2HfijdC3SqeT4cVHZBXW10r_QH9xHI");
+        fetch("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCxH2HfijdC3SqeT4cVHZBXW10r_QH9xHI", 
+        {
+            method: 'POST',
+            headers: {
+                "Content-Type": 'application/json'
+            },
+            
+        }
+        );
         dispatch({type: SIGNUP});
     };
 };
