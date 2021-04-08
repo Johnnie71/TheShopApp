360a72c57d51efd1365ee4af3a46e9e7e32dbad6
johnnie71 <johnnie.agonz71@gmail.com>
2021-04-08 10:48:08 -0400

    replaced key with API key

--- store/actions/auth.js
+++ store/actions/auth.js
@@ -2,6 +2,7 @@ export const SIGNUP = 'SIGNUP';
 
 export const signup = (email, password) => {
     return async dispatch => {
+        fetch("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCxH2HfijdC3SqeT4cVHZBXW10r_QH9xHI");
         dispatch({type: SIGNUP});
     };
 };
