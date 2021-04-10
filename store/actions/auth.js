fff6997e9eb3d116c81311a09b19e52075fc0869
johnnie71 <johnnie.agonz71@gmail.com>
2021-04-10 19:09:53 -0400

    url

--- store/actions/auth.js
+++ store/actions/auth.js
@@ -2,7 +2,8 @@ export const SIGNUP = 'SIGNUP';
 
 export const signup = (email, password) => {
     return async dispatch => {
-       const response = await fetch("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCxH2HfijdC3SqeT4cVHZBXW10r_QH9xHI", 
+       const response = await fetch(
+           "", 
         {
             method: 'POST',
             headers: {
