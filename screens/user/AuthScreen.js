bbf74c0edc8a2a1a3c03937c6942503b77a101f2
johnnie71 <johnnie.agonz71@gmail.com>
2021-04-13 18:25:49 -0400

    importing useEffect to allow us to show an error if we have an error

--- screens/user/AuthScreen.js
+++ screens/user/AuthScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useReducer, useCallback } from 'react';
+import React, { useState, useEffect, useReducer, useCallback } from 'react';
 import { 
     ScrollView, 
     View, 
