771c5bc5714886c1d8532aee0c4128fe79ef5809
johnnie71 <johnnie.agonz71@gmail.com>
2021-04-14 15:02:48 -0400

    returning token and user ID

--- store/reducers/auth.js
+++ store/reducers/auth.js
@@ -8,7 +8,15 @@ const initialState = {
 export default (state = initialState, action) => {
     switch (action.type) {
         case LOGIN:
+            return {
+                token: action.token,
+                userId: action.userId
+            };
         case SIGNUP:
+            return {
+                token: action.token,
+                userId: action.userId
+            };
             default: 
                 return state;
     }
