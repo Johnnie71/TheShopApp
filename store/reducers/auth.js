989b0b373d246bb009d78cc2190faabcd373d361
johnnie71 <johnnie.agonz71@gmail.com>
2021-04-14 14:59:43 -0400

    imported identifier for LOGIN

--- store/reducers/auth.js
+++ store/reducers/auth.js
@@ -1,8 +1,12 @@
+import { LOGIN } from "../actions/auth";
+
 const initialState = {
     token: null,
     userId: null
 };
 
 export default (state = initialState, action) => {
-    
+    switch (action.type) {
+        case LOGIN
+    }
 }
