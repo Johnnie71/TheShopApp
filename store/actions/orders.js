8faf283a5301179b84b0b030937f52f565978a1e
johnnie71 <johnnie.agonz71@gmail.com>
2021-04-15 15:57:44 -0400

    getState to get token

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -36,7 +36,8 @@ export const fetchOrders = () => {
 
 export const addOrder = (cartItems, totalAmount) => {
 
-    return async dispatch => {
+    return async (dispatch, getState) => {
+        const token = getState().auth.token;
         const date = new Date();
         const response = await fetch(
             'https://shopapp-759b2-default-rtdb.firebaseio.com/orders/u1.json',
