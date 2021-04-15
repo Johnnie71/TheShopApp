90f09d4ce4dcfd72076d4464c2cadec7ee6485df
johnnie71 <johnnie.agonz71@gmail.com>
2021-04-15 16:14:00 -0400

    changed default id to actual ownerId from action data

--- store/reducers/products.js
+++ store/reducers/products.js
@@ -16,8 +16,8 @@ export default (state = initialState, action) => {
             }
         case CREATE_PRODUCT: 
             const newProduct = new Product(
-               action.productData.id,
-                'u1', 
+                action.productData.id,
+                action.productData.ownerId, 
                 action.productData.title, 
                 action.productData.imageUrl, 
                 action.productData.description, 
