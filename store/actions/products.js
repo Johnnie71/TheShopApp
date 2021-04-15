64650f0b5259ad6d1a46a6bfcf83dac5e00802f9
johnnie71 <johnnie.agonz71@gmail.com>
2021-04-15 15:48:39 -0400

    getting token within fetch url

--- store/actions/products.js
+++ store/actions/products.js
@@ -93,10 +93,10 @@ export const createProduct = (title, description, imageUrl, price) => {
 
 export const updateProduct = (id, title, description, imageUrl) => {
     return async (dispatch, getState) => {
-      
+
      const token = getState().auth.token;
      const response = await fetch(
-        `https://shopapp-759b2-default-rtdb.firebaseio.com/products/${id}.json?auth=${}`, 
+        `https://shopapp-759b2-default-rtdb.firebaseio.com/products/${id}.json?auth=${token}`, 
         {
         method: "PATCH",
         headers: {
