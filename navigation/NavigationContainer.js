8906e28e21cf6abd34abc3af4663cc915914a177
johnnie71 <johnnie.agonz71@gmail.com>
2021-04-21 18:39:33 -0400

    returning shopnavigator

--- navigation/NavigationContainer.js
+++ navigation/NavigationContainer.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ShopNavigator from './ShopNavigator';
 
 const NavigationContainer = props => {
-
+    return <ShopNavigator />;
 };
 
 export default NavigationContainer;
