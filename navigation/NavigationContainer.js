fe72063b560691af39f0bb99a330d74fa682ec4c
johnnie71 <johnnie.agonz71@gmail.com>
2021-04-23 19:56:07 -0400

    deleted useEffect

--- navigation/NavigationContainer.js
+++ navigation/NavigationContainer.js
@@ -8,14 +8,6 @@ const NavigationContainer = props => {
     const navRef = useRef();
     const isAuth = useSelector(state => !!state.auth.token);
 
-    useEffect(() => {
-        if (!isAuth) {
-            navRef.current.dispatch(
-                NavigationActions.navigate({ routeName: 'Auth' })
-            );
-        }
-    }, [isAuth]);
-
     return <ShopNavigator ref={navRef} />;
 };
 
