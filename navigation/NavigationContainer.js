76665cc5d1e79ace61217469210afe59a34ced79
johnnie71 <johnnie.agonz71@gmail.com>
2021-04-23 20:00:36 -0400

    imported NavigationContainer from react navigation

--- navigation/NavigationContainer.js
+++ navigation/NavigationContainer.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { NavigationContainer } from '@react-navigation/native';
 
 import ShopNavigator from './ShopNavigator';
 
 const NavigationContainer = props => {
-    
     const isAuth = useSelector(state => !!state.auth.token);
 
-    return <ShopNavigator />;
+    return ;
 };
 
 export default NavigationContainer;
