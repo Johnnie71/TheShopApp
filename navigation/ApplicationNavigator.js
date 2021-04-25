6057578103139f06438462134782b763f09a64a0
johnnie71 <johnnie.agonz71@gmail.com>
2021-04-24 20:42:13 -0400

    got rid of imports

--- navigation/ApplicationNavigator.js
+++ navigation/ApplicationNavigator.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 
 // import ShopNavigator from './ShopNavigator';
 import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen';
 
-const MyStack = createStackNavigator();
 
 const ApplicationNavigator = props => {
     const isAuth = useSelector(state => !!state.auth.token);
