b73a57d67c9bba7e0dfdd87d0f3ec050fd9ab098
johnnie71 <johnnie.agonz71@gmail.com>
2021-04-29 16:20:22 -0400

    added another initial state for auto login attempt

--- store/reducers/auth.js
+++ store/reducers/auth.js
@@ -2,7 +2,8 @@ import { AUTHENTICATE, LOGOUT } from "../actions/auth";
 
 const initialState = {
     token: null,
-    userId: null
+    userId: null,
+    didTryAutoLogin: false
 };
 
 export default (state = initialState, action) => {
@@ -10,7 +11,8 @@ export default (state = initialState, action) => {
         case AUTHENTICATE:
             return {
                 token: action.token,
-                userId: action.userId
+                userId: action.userId,
+                didTryAutoLogin: true
             };
         case LOGOUT:
             return initialState;
