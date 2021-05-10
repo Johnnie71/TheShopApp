8525ac57647d9644284e9ddb83abe4e6e7792e10
johnnie71 <johnnie.agonz71@gmail.com>
2021-05-10 15:28:50 -0400

    accessing state to retrieve the pushToken

--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -22,7 +22,7 @@ const CartScreen = props => {
                     productPrice: state.cart.items[key].productPrice,
                     quantity: state.cart.items[key].quantity,
                     sum: state.cart.items[key].sum,
-                    productPushToken: 
+                    productPushToken: state.cart.items[key].pushToken
                 });
             }
             return transformedCartItems.sort((a, b) => a.productId > b.productId ?  1 : -1);
