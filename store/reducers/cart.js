b110e89bbad519c6b514a23b742cae930a52e3f7
johnnie71 <johnnie.agonz71@gmail.com>
2021-05-10 15:30:13 -0400

    const for push token when adding to cart

--- store/reducers/cart.js
+++ store/reducers/cart.js
@@ -14,6 +14,7 @@ export default (state = initialState, action) => {
             const addedProduct = action.product;
             const prodPrice = addedProduct.price;
             const prodTitle = addedProduct.title;
+            const pushToken = addedProduct.pushToken;
 
             let updatedOrNewCartItem;
 
